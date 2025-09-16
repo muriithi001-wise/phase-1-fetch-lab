3a10290ea23f14e1c91834cf3d04f502fa575a29
+ <+>
2025-09-16 14:21:46 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // To pass the tests, return the fetch Promise
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => {
+      renderBooks(books);
+    });
 }
 
 function renderBooks(books) {
